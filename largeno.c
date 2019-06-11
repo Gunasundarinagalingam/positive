2494230c236b05aaf8481ea18f03b6cfafa0dccf
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-06-11 20:20:43 +0530

    Create largeno.c

---
+++ largeno.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int num1,num2,num3;
+scanf("%d",&num1);
+scanf("%d",&num2);
+scanf("%d",&num3);
+if((num1>num2)&&(num1>num3))
+print("%d",num1);
+else if(num2<num3)
+print("%d",num2);
+else
+print("%d",num3);
