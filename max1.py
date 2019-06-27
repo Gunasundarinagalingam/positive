6fdc5444d3826930fae76a3752d0480362e9181d
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-06-27 19:54:33 +0530

    Update max1.py

--- max1.py
+++ max1.py
@@ -1,2 +1,7 @@
-number=list(map(int,input().split()))
-print(max(number))
+num=list(map(int,input().split()))
+g=num[0]
+for i in range(10):
+   for j in range(i+1,10):
+      if g < num{j]:
+                 g=num[j]
+print(g)              
