8d3d9a3b2f62b46ef78216b63b7f9125a9b281f7
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-06-28 12:26:15 +0530

    Create s58.py

---
+++ s58.py
@@ -0,0 +1,12 @@
+num=[int(i) for i in input().split()]
+mm=num[0]
+nn=num[1]
+num=[int(i) for i in input().split()]
+xx=0
+for i in num:
+  if i == nn:
+    xx=xx+1
+  if xx == 0:
+    print("no")
+  else:
+    print("yes")
