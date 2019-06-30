493cc583d0821c73bcd912448fd4c53c3429640c
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-06-30 13:01:59 +0530

    Update H9.py

--- H9.py
+++ H9.py
@@ -1,10 +1,9 @@
 ig,gi=map(int,input().split())
-ll=[]
-for i in range(ig+1,gi+1):
-  if i>1:
+l1=[]
+for i in range(ig,gi+1):
     for j in range(2,i):
-      if(i%j==0):
-        break
+        if i%j==0:
+            break
     else:
-      ll.append(j)
-print(len(l)+1)
+        l1.append(i)
+print(len(l1))
