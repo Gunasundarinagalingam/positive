3beb2a1f69dc79a1d10c0d3da5c385173312c6b4
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-06-30 13:39:16 +0530

    Create HH7.py

---
+++ HH7.py
@@ -0,0 +1,9 @@
+num1=int(input())
+num2=list(map(int,input().split()))
+l=[]
+for i in range(0,len(num2)):
+ if num2[i]%2==0 and i%2==1:
+  l.append(num2[i])
+ elif num2[i]%2==1 and i%2==0:
+  l.append(num2[i])
+print(*l,sep=' ')   
