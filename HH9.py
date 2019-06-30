d558967b90ae4c5627d231b0ed92240681d656c4
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-06-30 13:53:06 +0530

    Update HH9.py

--- HH9.py
+++ HH9.py
@@ -1,10 +1,11 @@
-num=int(input())
-num1=list(map(int,input().split()))
-oi=max(num1)
-x,y=0,0
-for i in range(0,len(num1)-1):
- for j in range(i+1,len(num1)):
-  if abs(num1[i]+num1[j])<oi:
-   x,y=num1[i],num1[j]
-   oi=abs(x+y)
-print(y,x)
+num1=int(input())
+arr=list(map(int,input().split()))
+c=len(arr)
+large=max(arr)
+y,z=0,0
+for i in range(0,c-1):
+ for j in range(i+1,c):
+  if abs(arr[i]+arr[j])< large:
+   y,z=arr[i],arr[j]
+   large=abs(y+z)
+print(y, z)
