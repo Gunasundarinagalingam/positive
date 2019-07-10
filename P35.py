1c396139d95b11d909301f83dcbbaa37132245db
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-07-10 21:00:45 +0530

    Update P35.py

--- P35.py
+++ P35.py
@@ -1,21 +1,21 @@
-vvj=input()
-lis=list(set(vj))
-xyy=1
-h=0
+nn1=input()
+ll1=list(set(nn1))
+x=1
+a=0
 check=False
 while True:
-    ch=lis[h]
-    for j in range(0,len(vvj)-xy):
-        if ch in vvj[j:j+xy]:
+    ch=ll1[a]
+    for y in range(0,len(nn1)-x):
+        if ch in nn1[y:y+x]:
             check=True
         else:
             check=False
-            h+=1
-            if h>=len(lis):
-             h=0
-             xyy+=1
+            a=a+1
+            if a>=len(ll1):
+             a=0
+             x=x+1
             break
 
     if check==True:
         break
-print(xyy)
+print(x)
