9534aac7dfdffb400c95da924bd4224d437a766d
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-07-19 13:25:36 +0530

    Update P46.py

--- P46.py
+++ P46.py
@@ -1,13 +1,13 @@
-vvj=int(input())
-vvk=list(map(int,input().split()))
-az=0
-by=0
-vvk.sort(reverse=True)
-for i in vvk:
-  vvk=az+i
-  if by>vvk:
-    az=vvk
+ppp3=int(input())
+qqq3=list(map(int,input().split()))
+a3=0
+b3=0
+qqq3.sort(reverse=True)
+for i in qqq3:
+  qqq3=a3+i
+  if b3>qqq3:
+    a3=qqq3
   else:
-    az=by
-    by=vvk
-print(az,by)
+    a3=b3
+    b3=qqq3
+print(a3,b3)
