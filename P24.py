b2fb08e5578fe7b045d183b82750ca46d30ab6be
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-07-19 12:05:25 +0530

    Update P24.py

--- P24.py
+++ P24.py
@@ -1,9 +1,9 @@
-aa1=int(input())
-bb1=pow(2,aa1)
+aaa1=int(input())
+bbb1=pow(2,aaa1)
 z1=[]
-for i in range(bb1):  
+for i in range(bbb1):  
     m1=bin(i).replace("0b","")
-    z1.append(m1.zfill(aa1))
+    z1.append(m1.zfill(aaa1))
     z1.sort(key=(lambda x:x.count('1')))
 for i in z1:
     print(i)
