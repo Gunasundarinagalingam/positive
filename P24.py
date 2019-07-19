6208355adc3a4f8f95e544b7195cfea1a5b9f1fc
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-07-19 11:39:50 +0530

    Update P24.py

--- P24.py
+++ P24.py
@@ -1,12 +1,9 @@
-nnn=int(input())
-nnn1=2**nnn
-ll1=[]
-for i in range(0,nnn1):
-    l=bin(i)[2:].zfill(nnn)
-    if(len(l)<len(bin(2**nnn-1)[2:])):
-        ll1.append([l.count("1"),l])
-    else:
-        ll1.append([l.count("1"),l])
-ll1.sort()
-for i in range(len(ll1)):
-    print(ll1[i][1])
+aa1=int(input())
+bb1=pow(2,aa1)
+z1=[]
+for i in range(bb1):  
+    m1=bin(i).replace("0b","")
+    z1.append(m1.zfill(aa1))
+    z1.sort(key=(lambda x:x.count('1')))
+for i in z1:
+    print(i)
