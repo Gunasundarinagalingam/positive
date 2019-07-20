d05c5a6f6ecbf8edb8eeeb38170712be0908a886
Gunasundarinagalingam <50632231+Gunasundarinagalingam@users.noreply.github.com>
2019-07-20 20:30:29 +0530

    Create HH75.py

---
+++ HH75.py
@@ -0,0 +1,8 @@
+ssm = int(input())
+lr = list(map(int,input().split()))
+for i in range(len(lr)-1):
+    if lr[i]>lr[i+1]:
+        print(lr[i+1],end=" ")
+    else:
+        print(-1,end=" ")
+print(-1)
